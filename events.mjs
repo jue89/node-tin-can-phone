f93f9078ba7f119524e66878d5560d29d4ba0a6c
Juergen Fitschen <me@jue.yt>
2024-01-05 22:23:24 +0100

    feat(events): add event emitter generator

--- events.mjs
+++ events.mjs
@@ -3,3 +3,7 @@ import {EVT} from './types.mjs';
 export function attachEventListener (onEvent, {recv}) {
 	recv([EVT], ([eventName, ...params]) => onEvent(eventName, ...params));
 }
+
+export function genEventEmitter ({send}) {
+	return (eventName, ...params) => send([EVT, eventName, ...params]);
+}
